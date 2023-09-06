147a83b79022ce381b12dba8cb95ebcbb3effd98
Cristina Alzuri <cristinaalzuri@gmail.com>
2023-09-05 21:50:07 -0400

    added redirect in the auth.js file and added time format in the helpers.js file

---
+++ utils/helpers.js
@@ -0,0 +1,6 @@
+module.exports = {
+    format_date: (date) => {
+      // Format date as MMMM Do, YYYY
+      return new Date(date).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
+    },
+  };
