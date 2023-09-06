fa8744db0d98a82990d84120e79afdc7472619c0
Cristina Alzuri <cristinaalzuri@gmail.com>
2023-09-05 22:20:59 -0400

    completed the comment and post models

---
+++ models/index.js
@@ -0,0 +1,35 @@
+// Import models
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// Defining associations between the models to establish relationships in the database.
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
