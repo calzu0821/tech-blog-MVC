8d8b0e97b9353c31cdeba7b18cafd3643ae8313a
Cristina Alzuri <cristinaalzuri@gmail.com>
2023-09-10 15:58:21 -0400

    added js for user logout

---
+++ public/js/logout.js
@@ -0,0 +1,16 @@
+const logout = async () => {
+    const response = await fetch('/api/users/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      // If logout is successful, redirect the browser to the homepage
+      document.location.replace('/');
+    } else {
+      // Display an alert with the error message if logout fails
+      alert(response.statusText);
+    }
+  };
+  
+  document.querySelector('#logout').addEventListener('click', logout);
